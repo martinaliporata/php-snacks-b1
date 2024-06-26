350df64c8036b4a1682eb5f61f14cbfdcc4cc335
martinaliporata <martinaliporata@gmail.com>
2024-06-26 12:20:02 +0200

    implementing array

---
+++ Snack 1/index.php
@@ -0,0 +1,26 @@
+<?php
+    $teams = [
+        [
+            "hometeam" => "rossi",
+            "visitingteam" => "gialli",
+            "pointshometeam" => 2,
+            "pointsvisitingteam" => 4,
+        ],
+        [
+            "hometeam" => "rossi"
+            "visiting team" => "gialli"
+            "points home team" => 
+            "points visiting team" =>
+        ],[
+            "hometeam" => "rossi"
+            "visiting team" => "gialli"
+            "points home team" => 
+            "points visiting team" =>
+        ],[
+            "hometeam" => "rossi"
+            "visiting team" => "gialli"
+            "points home team" => 
+            "points visiting team" =>
+        ],
+    ]
+?>
