abcdad7e227d4ac1c3e91549bc25dd2a3920ca49
martinaliporata <martinaliporata@gmail.com>
2024-06-26 18:16:48 +0200

    correct snack 1

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,34 +1,48 @@
 <?php
-    $teams = [
+    $matches = [
         [
-            "hometeam" => "rossi",
-            "visitingteam" => "gialli",
-            "pointshometeam" => 2,
-            "pointsvisitingteam" => 4,
+            "team1" => "rossi",
+            "team2" => "gialli",
+            "point_team_1" => 2,
+            "point_team_2" => 4,
         ],
         [
-            "hometeam" => "verdi",
-            "visitingteam" => "blu",
-            "pointshometeam" => 5,
-            "pointsvisitingteam" => 6,
-        ],[
-            "hometeam" => "fucsia",
-            "visitingteam" => "rosa",
-            "pointshometeam" => 7,
-            "pointsvisitingteam" => 2,
-        ],[
-            "hometeam" => "neri",
-            "visitingteam" => "grigi",
-            "pointshometeam" => 7,
-            "pointsvisitingteam" => 10,
+            "team1" => "verdi",
+            "team2" => "blu",
+            "point_team_1" => 5,
+            "point_team_2" => 6,
+        ],
+        [
+            "team1" => "fucsia",
+            "team2" => "rosa",
+            "point_team_1" => 7,
+            "point_team_2" => 2,
+        ],
+        [
+            "team1" => "neri",
+            "team2" => "grigi",
+            "point_team_1" => 7,
+            "point_team_2" => 10,
         ],
     ];
-
-    var_dump($teams);
-
-    foreach ($teams as $team) {
-        $team['pointshometeam'] = $randomNumber;
-    }
 ?>
 
-<!-- correzione -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <ul>
+            <?php foreach ($matches as $match) { ?>
+                <li>
+                    <?php echo $match['team1']; ?> - <?php echo $match['team2']; ?> | <?php echo $match['point_team_1']; ?> - <?php echo $match['point_team_2']; ?>
+                </li>
+            <?php } ?>
+        </ul>
+    </main>
+</body>
+</html>
