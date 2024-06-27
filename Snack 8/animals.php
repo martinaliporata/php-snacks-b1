ce960f859be241ee9cde70bee9f750b07631c726
martinaliporata <martinaliporata@gmail.com>
2024-06-27 16:12:48 +0200

    implementing snack 8

--- Snack 8/animals.php
+++ Snack 8/animals.php
@@ -1,31 +1,44 @@
 <?php
 $animals = [
-        ["nome" => "lino",
-        "specie" => "cavallo",
-        "classe" => "mammifero"],
-        ["nome" => "lino",
-        "specie" => "cane",
-        "classe" => "mammifero"],
-        ["nome" => "lino",
-        "specie" => "anguilla",
-        "classe" => "pesce"],
-        ["nome" => "lino",
-        "specie" => "merluzzo",
-        "classe" => "pesce"],
-        ["nome" => "lino",
-        "specie" => "trota",
-        "classe" => "pesce"],
-        ["nome" => "lino",
-        "specie" => "gatto",
-        "classe" => "mammifero"],
-        ["nome" => "lino",
-        "specie" => "rana",
-        "classe" => "anfibio"],
-        ["nome" => "lino",
-        "specie" => "scimmia",
-        "classe" => "mammifero"],
-        ["nome" => "lino",
-        "specie" => "serpente",
-        "classe" => "rettile"],
+        [
+            "nome" => "lino",
+            "specie" => "cavallo",
+            "classe" => "mammifero"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "cane",
+            "classe" => "mammifero"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "anguilla",
+            "classe" => "pesce"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "merluzzo",
+            "classe" => "pesce"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "trota",
+            "classe" => "pesce"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "rana",
+            "classe" => "anfibio"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "iguana",
+            "classe" => "rettile"
+        ],
+        [
+            "nome" => "lino",
+            "specie" => "serpente",
+            "classe" => "rettile"
+        ],
     ]
 ?>
--- Snack 8/index.php
+++ Snack 8/index.php
@@ -1,3 +1,63 @@
 <?php
-    include __DIR__ . "/arrays"
+    require_once __DIR__ . "/animals.php";
+
+    $mammiferi = array_filter(
+        $animals, function ($animal) {
+            return $animal["classe"] == "Mammifero";
+        }
+    );
+
+    $pesci = array_filter(
+        $animals, function ($animal) {
+            return $animal["classe"] == "Pesce";
+        }
+    );
+
+    $anfibi = array_filter(
+        $animals, function ($animal) {
+            return $animal["classe"] == "Anfibio";
+        }
+    );
+
+    $rettili = array_filter(
+        $animals, function ($animal) {
+            return $animal["classe"] == "Rettile";
+        }
+    );
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <ul>
+        <!-- al posto di $mammifero a riga 420 e 42, devo scrivere animal? -->
+        <?php foreach ($mammiferi as $mammifero) { ?>
+            <li>
+                <?php echo $mammifero["nome"] . " " . $mammifero["classe"]; ?>
+            </li>
+        <?php } ?>
+        <?php foreach ($pesci as $pesce) { ?>
+            <li>
+                <?php echo $pesce["nome"] . " " . $pesce["classe"]; ?>
+            </li>
+        <?php } ?>
+        <?php foreach ($anfibi as $anfibio) { ?>
+            <li>
+                <?php echo $anfibio["nome"] . " " . $anfibio["classe"]; ?>
+            </li>
+        <?php } ?>
+        <?php foreach ($rettili as $rettile) { ?>
+            <li>
+                <?php echo $rettile["nome"] . " " . $rettile["classe"]; ?>
+            </li>
+        <?php } ?>
+    </ul>
+    </main>
+</body>
+</html>
