0cde917d57da31ca79bca7cd6f7271af7fa568a0
martinaliporata <martinaliporata@gmail.com>
2024-06-27 14:36:19 +0200

    correct snack 2

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,21 +1,16 @@
 <?php
-    if ($_GET[name] > 3) {
-        echo "Accesso riuscito";
-    } else {
+// se la lunghezza della stringa inserita nell'input name di get è minore o uguale a 3, allora nega l'accesso
+    if (strlen($_GET["name"]) <= 3) {
         echo "Accesso negato";
-    };
-
-    if (strpos($_GET[mail], "@" && ".", offset)) {
-        echo "Accesso riuscito";
-    } else {
+// se nella stringa mail inserita nell'input mail di get non c'è né la @ né il ., allora nega l'accesso
+    } elseif (!str_contains($_GET["mail"], "@") || !str_contains($_GET["mail"], ".")) {
         echo "Accesso negato";
-    };
-    
-    if (is_numeric($_GET[age])) {
-        echo "Accesso riuscito";
-    } else {
+// se il numero della stringa inserita nell'input age di get non è un numero, allora nega l'accesso
+    } elseif (!is_numeric($_GET["age"])) {
         echo "Accesso negato";
-    } 
+    } else {
+        echo "Accesso riuscito";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -27,12 +22,23 @@
 </head>
 <body>
     <form action="./index.php" method="get">
-        <input type="text" name="name" id="name">
-        <input type="text" name="mail" id="mail">
-        <input type="text" name="age" id="age">
-        <button type="submit">
-            Invia
-        </button>
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="mail">Mail</label>
+            <input type="text" name="mail" id="mail">
+        </div>
+        <div>
+            <label for="age">Age</label>
+            <input type="text" name="age" id="age">
+        </div>
+        <div>
+            <button type="submit">
+                Invia
+            </button>
+        </div>
     </form>
 </body>
 </html>
