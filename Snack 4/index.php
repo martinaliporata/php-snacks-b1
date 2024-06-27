5394462f1adf8340a7a42889ed4a28db19f94963
martinaliporata <martinaliporata@gmail.com>
2024-06-27 14:59:59 +0200

    correct snack 4

--- Snack 4/index.php
+++ Snack 4/index.php
@@ -1,12 +1,22 @@
 <?php    
-    
     $randomNumbers = [];
 
+    // finché la lenght di randonNumbers è minore di 15, creami numeri randomici
     while (count($randomNumbers) < 15) {
         $singleNum = rand(1, 20);
-
-        if($singleNum, $randomNumbers) {
-            
+        // cosa cerco? SingleNum. Dove? In randomNumbers. Quindi qui dico "se non c'è..."
+        if(!in_array($singleNum, $randomNumbers)) {
+            // Allora fammi un array_push in randomNumber di singleNum
+            array_push($randomNumbers, $singleNum);
         }
     }
 ?>
+
+
+<ul>
+    <?php foreach ($randomNumbers as $number) { ?>
+        <li>
+            <?php echo $number ?>
+        </li>
+    <?php } ?>
+</ul>
