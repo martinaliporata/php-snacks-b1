3534d04be6df7c07e83261b3c677b22c24b894ca
martinaliporata <martinaliporata@gmail.com>
2024-07-03 12:28:50 +0200

    implementing extends

--- Snack 9/Membership.php
+++ Snack 9/Membership.php
@@ -1,3 +1,5 @@
 <?php
-
+    class membership extends premiumUser{
+        private $gift;
+    }
 ?>
--- Snack 9/User.php
+++ Snack 9/User.php
@@ -8,5 +8,14 @@
             $this->name= $name;
             $this->surname= $surname;
         }
+
+    public function getFullName(){
+        return "Ciao, io sono {$this->name} {this->surname}";
     }
+
+    public function setFullName($name, $surname) {
+        $this->name=$name;
+        $this->surname=$surname;
+    }
+}
 ?>
