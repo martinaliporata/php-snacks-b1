c7beb7f3ab15b64dcee65455517117f40db6ccef
martinaliporata <martinaliporata@gmail.com>
2024-07-03 12:15:41 +0200

    added construct

--- Snack 9/User.php
+++ Snack 9/User.php
@@ -1,3 +1,12 @@
 <?php
+    class User {
+        private $name;
+        private $surname;
 
+    public function __construct($name, $surname)
+        {
+            $this->name= $name;
+            $this->surname= $surname;
+        }
+    }
 ?>
